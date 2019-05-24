fbc2ffcba69bf29e3699a5b9aa5469b9a7929534
FERNANDO BASTEIRO <FERNANDOBASTEIRO@DESKTOP-HE8NFV2.argentina.ibm.com>
2019-05-24 16:08:31 -0300

    Un poco mas de responsividad.

--- Web/WebContent/assets/js/custom.js
+++ Web/WebContent/assets/js/custom.js
@@ -1,7 +1,15 @@
  function loadDiv(divName, url, postInfo) {
+	 	var w = window.innerWidth;
+		if (w < 768) {
+			document.getElementById("accordionSidebar").className += " toggled";
+			document.getElementById("page-top").className += " sidebar-toggled";
+			document.getElementById("collapse1").className = "collapse";
+		}
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {document.getElementById(divName).innerHTML = this.responseText;}};
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(divName).innerHTML = this.responseText;
+			}};
 	    xmlhttp.open("POST", url, true);
 		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	    xmlhttp.send(postInfo);
