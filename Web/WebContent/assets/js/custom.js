386dbe0a1779bd2c24707037f860ed4805faac1e
FERNANDO BASTEIRO <FERNANDOBASTEIRO@DESKTOP-HE8NFV2.argentina.ibm.com>
2019-05-25 14:58:28 -0300

    Pruebas con DatePicker y cambios de textos varios

--- Web/WebContent/assets/js/custom.js
+++ Web/WebContent/assets/js/custom.js
@@ -1,4 +1,4 @@
- function loadDiv(divName, url, postInfo) {
+ function loadDiv(divName, url, postInfo, titulo) {
 	 	var w = window.innerWidth;
 		if (w < 768) {
 			document.getElementById("accordionSidebar").className += " toggled";
@@ -9,6 +9,8 @@
 		xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById(divName).innerHTML = this.responseText;
+			document.getElementById("titulo-pagina").innerHTML  = titulo;
+			document.getElementById("titulo-pagina-chica").innerHTML  = titulo;
 			}};
 	    xmlhttp.open("POST", url, true);
 		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
